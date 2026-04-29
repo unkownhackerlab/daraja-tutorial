d3575bda5a86337049485bbbdc27a2ee0ed15183
404unkown <uhacker455@gmail.com>
2026-04-29 11:48:20 +0300

    Update stk_initiate.php

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,12 +5,12 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'nk16Y74eSbTaGQgc9WF8j6FigApqOMWr'; //Fill with your app Consumer Key
-  $consumerSecret = '40fD1vRXCq90XFaU'; // Fill with your app Secret
+  $consumerKey = 'jFELg05eVRkC3GRvVLRKCIUzuVgcR9fNtW4VsdqG9HL7UpVClBDZIQHGbhPi7PH6'; //Fill with your app Consumer Key
+  $consumerSecret = 'leLyX6WqZUxJIgH4A4MlzjlEzlqRIQpjCl1qATclshu22nd4'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
-  $BusinessShortCode = '174379';
+  $BusinessShortCode = '5719503';
   $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';  
   
   /*
